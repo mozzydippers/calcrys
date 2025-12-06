12629094b82409fc88594acfeef09cff53a77d43
Aero <47219799+Aeliko@users.noreply.github.com>
2025-12-06 15:57:32 +0800

    Fix text number after merge

--- data/battle_scripts/subscripts/subscript_0473_Z_DANCE_AND_EFFECT.s
+++ data/battle_scripts/subscripts/subscript_0473_Z_DANCE_AND_EFFECT.s
@@ -6,11 +6,11 @@ _000:
     PlayBattleAnimation BATTLER_CATEGORY_ATTACKER, BATTLE_ANIMATION_Z_DANCE
     Wait
     //{0} surrounded itself with its Z-Power!
-    PrintMessage 1597, TAG_NICKNAME, BATTLER_CATEGORY_ATTACKER
+    PrintMessage 1603, TAG_NICKNAME, BATTLER_CATEGORY_ATTACKER
     Wait
     WaitButtonABTime 30
     //{0} unleashes its full-force Z-Move!
-    PrintMessage 1594, TAG_NICKNAME, BATTLER_CATEGORY_ATTACKER
+    PrintMessage 1600, TAG_NICKNAME, BATTLER_CATEGORY_ATTACKER
     Wait
     WaitButtonABTime 30
     UpdateVar OPCODE_FLAG_OFF, BSCRIPT_VAR_BATTLE_STATUS, BATTLE_STATUS_MOVE_ANIMATIONS_OFF
