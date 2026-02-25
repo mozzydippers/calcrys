badebd3280c06d813d9486df8120f23bc698d697
Aero <47219799+Aeliko@users.noreply.github.com>
2026-02-25 22:29:59 +0800

    Consolidate common battle variation logic

--- src/battle_variation.c
+++ src/battle_variation.c
@@ -5,10 +5,47 @@
 #include "../include/types.h"
 
 // Pokemon generated will be illegal
+struct PartyPokemon *InitialiseBattleVariationEnemy(void *taskManager, struct BATTLE_PARAM *setup, struct BattleVariationBase *battleVariationBase)
+{
+    FieldSystem *fieldSystem = TaskManager_GetFieldSystem(taskManager);
+
+    BattleSetup_InitFromFieldSystem(setup, fieldSystem);
+    ov02_02247F30(fieldSystem, battleVariationBase->mainPokemon.species, battleVariationBase->mainPokemon.level, battleVariationBase->mainPokemon.forceShiny, setup);
+
+    struct PartyPokemon *mon = Party_GetMonByIndex(setup->poke_party[BATTLER_ENEMY], 0);
+
+    u32 personality = ChangePersonalityToNatureGenderAndAbility(42, battleVariationBase->mainPokemon.species, battleVariationBase->mainPokemon.nature, battleVariationBase->mainPokemon.gender, battleVariationBase->mainPokemon.abilitySlot, 0);
+
+    SetMonPersonality(mon, personality);
+
+    SetMonData(mon, MON_DATA_HELD_ITEM, &battleVariationBase->mainPokemon.heldItem);
+
+    int maxPP;
+    for (int i = 0; i < 4; i++) {
+        SetMonData(mon, MON_DATA_MOVE1 + i, &battleVariationBase->mainPokemon.moves[i]);
+        maxPP = GetMonData(mon, MON_DATA_MOVE1MAXPP + i, 0);
+        SetMonData(mon, MON_DATA_MOVE1PP + i, &maxPP);
+    }
+
+    for (int i = 0; i < 6; i++) {
+        SetMonData(mon, MON_DATA_HP_EV + i, &battleVariationBase->mainPokemon.basePoints[i]);
+    }
+
+    for (int i = 0; i < 6; i++) {
+        SetMonData(mon, MON_DATA_HP_IV + i, &battleVariationBase->mainPokemon.potential[i]);
+    }
+
+    RecalcPartyPokemonStats(mon);
+
+    GameStats_Inc(Save_GameStats_Get(fieldSystem->savedata), 8);
+
+    return mon;
+}
+
 void LONG_CALL SetupAndStartTotemBattle(void *taskManager, u32 *winFlag, u16 battleID)
 {
     struct BATTLE_PARAM *setup;
-    FieldSystem *fieldSystem = TaskManager_GetFieldSystem(taskManager);
+
     SetScriptVar(RAID_ID_VARIABLE, battleID);
 
     struct TotemBattle battleData;
@@ -16,35 +53,10 @@ void LONG_CALL SetupAndStartTotemBattle(void *taskManager, u32 *winFlag, u16 bat
     ArchiveDataLoadOfs(&battleData, ARC_CODE_ADDONS, CODE_ADDON_TOTEMBATTLES, battleID * sizeof(struct TotemBattle), sizeof(struct TotemBattle));
 
     setup = BattleSetup_New(HEAPID_WORLD, BATTLE_TYPE_SINGLE);
-    BattleSetup_InitFromFieldSystem(setup, fieldSystem);
-    ov02_02247F30(fieldSystem, battleData.battleVariationBase.mainPokemon.species, battleData.battleVariationBase.mainPokemon.level, battleData.battleVariationBase.mainPokemon.forceShiny, setup);
 
     setup->battleSpecial |= BATTLE_SPECIAL_TOTEM;
 
-    struct PartyPokemon *mon = Party_GetMonByIndex(setup->poke_party[BATTLER_ENEMY], 0);
-
-    u32 personality = ChangePersonalityToNatureGenderAndAbility(42, battleData.battleVariationBase.mainPokemon.species, battleData.battleVariationBase.mainPokemon.nature, battleData.battleVariationBase.mainPokemon.gender, battleData.battleVariationBase.mainPokemon.abilitySlot, 0);
-
-    SetMonPersonality(mon, personality);
-
-    SetMonData(mon, MON_DATA_HELD_ITEM, &battleData.battleVariationBase.mainPokemon.heldItem);
-
-    int maxPP;
-    for (int i = 0; i < 4; i++) {
-        SetMonData(mon, MON_DATA_MOVE1 + i, &battleData.battleVariationBase.mainPokemon.moves[i]);
-        maxPP = GetMonData(mon, MON_DATA_MOVE1MAXPP + i, 0);
-        SetMonData(mon, MON_DATA_MOVE1PP + i, &maxPP);
-    }
-
-    for (int i = 0; i < 6; i++) {
-        SetMonData(mon, MON_DATA_HP_EV + i, &battleData.battleVariationBase.mainPokemon.basePoints[i]);
-    }
-
-    for (int i = 0; i < 6; i++) {
-        SetMonData(mon, MON_DATA_HP_IV + i, &battleData.battleVariationBase.mainPokemon.potential[i]);
-    }
-
-    GameStats_Inc(Save_GameStats_Get(fieldSystem->savedata), 8);
+    InitialiseBattleVariationEnemy(taskManager, setup, &battleData.battleVariationBase);
 
     CallTask_StartEncounter(taskManager, setup, BattleSetup_GetWildTransitionEffect(setup), BattleSetup_GetWildBattleMusic(setup), winFlag);
 }
