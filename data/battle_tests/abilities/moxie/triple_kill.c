3884fa0b3e29a90ce7777cf312b9165ee0b54345
kumakuma1 <kuehnmaxi@gmail.com>
2026-03-25 14:26:53 +0100

    add moxie test

---
+++ data/battle_tests/abilities/moxie/triple_kill.c
@@ -0,0 +1,140 @@
+// Test: Moxie - Triple Kill
+#ifndef GET_TEST_CASE_ONLY
+
+#include "../../../../include/battle.h"
+#include "../../../../include/constants/ability.h"
+#include "../../../../include/constants/item.h"
+#include "../../../../include/constants/moves.h"
+#include "../../../../include/constants/species.h"
+#include "../../../../include/test_battle.h"
+
+const struct TestBattleScenario BattleTests[] = {
+
+#endif
+
+    {
+        .battleType = BATTLE_TYPE_DOUBLE,
+        .weather = WEATHER_NONE,
+        .fieldCondition = 0,
+        .terrain = TERRAIN_NONE,
+        .playerParty = {
+            {
+                .species = SPECIES_GYARADOS,
+                .level = 50,
+                .form = 0,
+                .ability = ABILITY_MOXIE,
+                .item = ITEM_LIFE_ORB,
+                .moves = { MOVE_EARTHQUAKE, MOVE_NONE, MOVE_NONE, MOVE_NONE },
+                .hp = FULL_HP,
+                .status = 0,
+                .condition2 = 0,
+                .moveEffectFlags = 0,
+            },
+            {
+                .species = SPECIES_CHARMANDER,
+                .level = 20,
+                .form = 0,
+                .ability = ABILITY_BLAZE,
+                .item = ITEM_NONE,
+                .moves = { MOVE_SPLASH, MOVE_NONE, MOVE_NONE, MOVE_NONE },
+                .hp = FULL_HP,
+                .status = 0,
+                .condition2 = 0,
+                .moveEffectFlags = 0,
+            },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE } },
+        .enemyParty = { {
+                            .species = SPECIES_CYNDAQUIL,
+                            .level = 20,
+                            .form = 0,
+                            .ability = ABILITY_BLAZE,
+                            .item = ITEM_NONE,
+                            .moves = { MOVE_SPLASH, MOVE_NONE, MOVE_NONE, MOVE_NONE },
+                            .hp = FULL_HP,
+                            .status = 0,
+                            .condition2 = 0,
+                            .moveEffectFlags = 0,
+                        },
+            {
+                .species = SPECIES_TORCHIC,
+                .level = 20,
+                .form = 0,
+                .ability = ABILITY_BLAZE,
+                .item = ITEM_NONE,
+                .moves = { MOVE_SPLASH, MOVE_NONE, MOVE_NONE, MOVE_NONE },
+                .hp = FULL_HP,
+                .status = 0,
+                .condition2 = 0,
+                .moveEffectFlags = 0,
+            },
+            {
+                .species = SPECIES_CHIMCHAR,
+                .level = 20,
+                .form = 0,
+                .ability = ABILITY_BLAZE,
+                .item = ITEM_NONE,
+                .moves = { MOVE_SPLASH, MOVE_NONE, MOVE_NONE, MOVE_NONE },
+                .hp = FULL_HP,
+                .status = 0,
+                .condition2 = 0,
+                .moveEffectFlags = 0,
+            },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE },
+            { .species = SPECIES_NONE } },
+        .playerScript = { {
+                              { ACTION_MOVE_SLOT_1, BATTLER_ENEMY_FIRST },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                              { ACTION_NONE, 0 },
+                          },
+            {
+                { ACTION_MOVE_SLOT_1, BATTLER_ENEMY_FIRST },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+            } },
+        .enemyScript = { {
+                             { ACTION_MOVE_SLOT_1, BATTLER_PLAYER_FIRST },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                             { ACTION_NONE, 0 },
+                         },
+            {
+                { ACTION_MOVE_SLOT_1, BATTLER_PLAYER_FIRST },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+                { ACTION_NONE, 0 },
+            } },
+        .expectations = {
+            { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.message = "Delphox used Incinerate!" },
+            { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.message = "Charmander fainted!" },
+            { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.message = "The opposing Cyndaquil fainted!" },
+            { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.message = "The opposing Torchic fainted!" },
+            { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.message = "Gyarados’s Moxie raised its Attack drastically!" },
+            { .expectationType = EXPECTATION_TYPE_MESSAGE, .expectationValue.message = "Gyarados lost some of its HP!" },
+        },
+    },
+#ifndef GET_TEST_CASE_ONLY
+};
+// each test file is a separate .c file in battle_tests/ for better organization
+#endif
